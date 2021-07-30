fc01d3f2d3c7acef378ae2020302f7169b5fd2a9
NIHARIKA983 <niharikagowda1996@gmail.com>
2021-07-30 12:27:19 +0530

    Added:Ability to analyse and respond Happy or Sad Mood

---
+++ src/main/java/com/bridgelabz/MoodAnalyser.java
@@ -0,0 +1,31 @@
+package com.bridgelabz;
+
+public class MoodAnalyser {
+    public String MoodAnalyser(String s) {
+        if (s.contains(("sad"))) {
+            System.out.println("Test Pass");
+
+            return "sad";
+
+        }
+        else if (s.contains(("i am in happy mood"))){
+            return "sad";
+        }
+
+        return "Happy";
+    }
+
+    public String analyseMood(String message) {
+        if (message.contains(("sad"))) {
+            System.out.println("Test Pass");
+            return "sad";
+        } else if (message.contains(("i am sad mood"))) {
+            System.out.println("Test Pass");
+            return "SAD";
+        } else if (message.contains(("i am in any mood"))) {
+            System.out.println("Test Pass");
+            return "Happy";
+        } else return message;
+    }
+
+}
---
+++ src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -0,0 +1,44 @@
+package com.bridgelabz;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MoodAnalyserTest {
+    MoodAnalyser moodAnalyser = new MoodAnalyser();
+
+    @Test
+    public void testMoodAnalysis() {
+        String mood = moodAnalyser.analyseMood("sad");
+        Assert.assertEquals("sad", mood);
+
+    }
+
+    // Method calling through the instences
+    @Test
+    public void TestCase1() {
+        String mood = moodAnalyser.analyseMood("i am sad mood");
+        Assert.assertEquals("sad", mood);
+    }
+
+    @Test
+    public void TestCase2() {
+        String mood = moodAnalyser.analyseMood("i am in any mood");
+        Assert.assertEquals("Happy", mood);
+    }
+    // method calling through the constructor
+
+    @Test
+    public void TestCase1Repete() {
+        //String mood = moodAnalyser.analyseMood("i am sad mood");
+        String mood = moodAnalyser.MoodAnalyser("i am in sad");
+        Assert.assertEquals("sad", mood);
+    }
+    @Test
+    public void TestCase2Repete() {
+        //String mood = moodAnalyser.analyseMood("i am sad mood");
+        String mood = moodAnalyser.MoodAnalyser("i am in happy mood");
+        Assert.assertEquals("sad", mood);
+    }
+
+}
+
